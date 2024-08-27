f8ffedf513bd7bdb228861f1ee4ade7bb1c8fecf
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-08-28 00:52:46 +0500

    Hello World

--- Hello World/src/App.jsx
+++ Hello World/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="background">
-    <h1 className="text">Hello React With <abbr title="Muhammad Subhan Khan">MSK</abbr></h1>
+    <h1 className="text">Hello React with <abbr title="Muhammad Subhan Khan">Subhan</abbr></h1>
     </div>
   )
 }
