1689a5e032a7ccd4255c79800a8a75d449620a22
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-08-29 01:42:05 +0500

    React Components

---
+++ React-js Components/src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar =  ()=>{
+
+    return(
+        <>
+        <div className="navbar text-2xl w-5 mt-3">
+            <h2>Products</h2>
+            <nav className="flex justify-end gap-2">
+            <a href="">Shoes</a>
+            <a href="">Mobiles</a>
+            <a href=""></a>
+            <a href=""></a>
+
+            </nav>
+        </div>
+        </>
+    )
+}
+
+export default Navbar();
