60be4980a37afa5e27775b64fed3b5151ee975cd
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-08-29 23:47:17 +0500

    React Component

--- React-Components/src/App.jsx
+++ React-Components/src/App.jsx
@@ -8,6 +8,11 @@ function App() {
 
   return (
     <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    
 <h1>Components</h1>
 <Button/>
 <Navbar/>
--- React-Components/src/components/button.jsx
+++ React-Components/src/components/button.jsx
@@ -3,14 +3,7 @@
 
 function Button() {
     return(
-        <button style={{
-            backgroundColor : "green",
-            padding: "1rem",
-            color:"whitesmoke",
-            alignItems:"center",
-            fontSize: "20px"
-
-        }}>Click</button>
+        <button className="btn bg-neutral-950 text-white">Click</button>
     )
 }
 
