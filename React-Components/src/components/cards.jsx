038a811c4445a7d409131aab47e782d12c4a35fb
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-08-29 23:55:22 +0500

    React Components

--- React-Components/src/components/cards.jsx
+++ React-Components/src/components/cards.jsx
@@ -2,12 +2,15 @@
 
 function Cards() {
     return(
-        <h1 style={{
-            textAlign: "center",
-            backgroundColor : "Red",
-            color : "white",
-            padding: "2rem",
-        }} >Cards hen yeh.....</h1>
+
+        <div class="cards-container">
+            <div class="card">
+                <img src="https://via.placeholder.com/150" alt="Card Image"/>
+                <h2>Card Title 1</h2>
+                <p>This is the description of the card. It scales up slightly on hover.</p>
+                <button class="card-btn">Learn More</button>
+            </div>
+        </div>
     )
 }
 
--- React-Components/src/components/navbar.jsx
+++ React-Components/src/components/navbar.jsx
@@ -4,13 +4,18 @@ function Navbar() {
     
 
     return(
-        <h1 style={{
-            textAlign: "center",
-            backgroundColor : "blue",
-            color : "white",
-            padding: "2rem",
-        }} 
-        >Navbar ha yeh</h1>
+        // <  <!-- Navbar -->
+        <nav class="navbar">
+            <div class="navbar-content">
+                <a href="#" class="navbar-brand">3D Navbar</a>
+                <ul class="navbar-menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
