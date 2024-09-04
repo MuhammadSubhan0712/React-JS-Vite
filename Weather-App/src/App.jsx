3e842d89905bcb9eb91abce360682a7891d4b9eb
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-05 02:14:18 +0500

    Weather App

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -5,7 +5,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [data, setdata] = useState([])
+  const [Weatherdata, setWeatherdata] = useState([])
   const card = useRef()
 
   useEffect(()=>{
@@ -14,9 +14,10 @@ function App() {
   //   `https://api.weatherapi.com/v1/current.json?key=569c0fa5727f4e9387350542241406 &q=${input.value}&aqi=no`
   // )
     
-axios("https://api.weatherapi.com/v1/current.json?key=569c0fa5727f4e9387350542241406 &q=&aqi=no")
+axios(`https://api.weatherapi.com/v1/current.json?key=569c0fa5727f4e9387350542241406 &q=${card}&aqi=no`)
 .then((res) =>{
-   console.log(res.data)})
+   console.log(res.data)}
+  )
 
 .catch((err) =>{
   console.log("Error==>", err)})
@@ -36,8 +37,8 @@ axios("https://api.weatherapi.com/v1/current.json?key=569c0fa5727f4e938735054224
           backgroundColor:"gray"
         }}>W E A T H E R || A P P </h1>
 
-        <form >
-          <input type="text" placeholder='Enter City to Check Weather' ref={data}/>
+        <form onSubmit={Card} >
+          <input type="text" placeholder='Enter City to Check Weather' ref={card}/>
         </form>
 
         </div>
