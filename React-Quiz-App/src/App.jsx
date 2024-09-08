93fc51a8d7d97be0ee4e009f825065fec2118efd
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-08 14:58:18 +0500

    Quiz-App

--- React-Quiz-App/src/App.jsx
+++ React-Quiz-App/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Question, setQuestion] = useState([])
+  const [QuestionState , setQuestionState] = useState(0)
+
+  const checkinput = useRef([])
+
+  useEffect(() => {
+
+    axios("https://the-trivia-api.com/v2/questions")
+    .then((res) => { 
+      console.log(res.data);
+      setQuestion(res.data);
+    })
+
+    .catch((error) => { 
+      console.log("Error ==> " , error);
+      
+    })
+  }, [])
+
+function Shuffling(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)) ;
+
+    [array[i] , array[j] = array[j] , array[i]]
+  }
+  return array
+}
+
+
+  function nextQuestion(index) {
+    const checkedBtn =  checkinput.current.find(input => input.checked);
+    if (checkedBtn) {
+      const selectedval = checkedBtn.value;
+      console.log("Selected Answer ==>" , selectedval);
+    }
+    QuestionState < Question.length - 1 ? setQuestionState(Question + 1) : alert("Questions End");
+  }
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Quiz App</h1>
+      {Question.length > 0 ? <div>
+        <h1>Q{QuestionState + 1}: {Question[QuestionState].Question.text}</h1>
+        <ul>
+          {Shuffling([...Question[QuestionState].incorrectAnswers , Question[QuestionState].correctAnswer]).map((item , index)=>{
+            return <li key={index}>
+            <input type="radio" name='choice' id={item} value={item} ref={el => (checkinput.current[index] = el)}/>
+            <label htmlFor={item}>{item}</label>
+          </li>
+          
+          })}
+        </ul>
+        <button onClick={()=> nextQuestion()}>Next {Question.length}</button>
+      </div> : <h1>Loading...</h1>}
     </>
+
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
