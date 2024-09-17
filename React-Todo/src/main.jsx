52f00b60cf1ca58e0c395c8aa2b873b8af40a348
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-17 07:41:15 +0500

    React-Todo / Firebase / Tailwindcss

--- React-Todo/src/main.jsx
+++ React-Todo/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter[ {
+path: "/",
+element: <Login />,
+} ]
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<RouterProvider router={router} >
+<App />
+</RouterProvider>
+  
 )
