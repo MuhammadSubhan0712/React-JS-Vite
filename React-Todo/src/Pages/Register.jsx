b8d33a252d72e903893e0bee714c94377dcb4b3c
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-21 01:30:43 +0500

    Registration Completed

--- React-Todo/src/Pages/Register.jsx
+++ React-Todo/src/Pages/Register.jsx
@@ -123,7 +123,8 @@ const Registration = () => {
 
           {/* Submit Button */}
           <div className="form-control mt-6">
-            <button type="submit"
+            <button
+              type="submit"
               className="w-full flex justify-center py-3 px-4 border border-transparent rounded-lg shadow-lg text-white bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
               Register
             </button>
