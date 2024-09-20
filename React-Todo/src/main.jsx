2dc6fe7a2cdde63bec36428962760abed6bc6c4b
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-21 01:31:15 +0500

    Completed

--- React-Todo/src/main.jsx
+++ React-Todo/src/main.jsx
@@ -1,34 +1,32 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Login from './Pages/Login.jsx'
-import Registration from './Pages/Register.jsx'
-import Todo from './Pages/Todo.jsx'
+import Login from "./Pages/Login.jsx";
+import Registration from "./Pages/Register.jsx";
+import Todo from "./Pages/Todo.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Registration />,
+  },
+  {
+    path: "todo",
+    element: <Todo />,
+  },
+  {
+    path: "*",
+    element: (
+      <h2 className="flex justify-center mt-10 p-5 text-3xl">Not Found</h2>
+    ),
+  },
+]);
 
-
-const router = createBrowserRouter([ 
-    {
-    path:"/",
-    element:<Login/>,
-    },
-    {
-      path: "register",
-      element: <Registration/>,
-    }, 
-    {
-      path:"todo",
-      element:<Todo/>
-    },
-{
-  path: "*",
-  element: <h2 className='flex justify-center mt-10 p-5 text-3xl'>Not Found</h2>
-}
-])
-
-
-createRoot(document.getElementById('root')).render(
-<RouterProvider router={router}>
-</RouterProvider>
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
