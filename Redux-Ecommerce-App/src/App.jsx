8160ae8488d2f7b6fa2d9b946a4ce20b9f943697
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-26 02:44:18 +0500

    Ecommerce-App

--- Redux-Ecommerce-App/src/App.jsx
+++ Redux-Ecommerce-App/src/App.jsx
@@ -1,11 +1,24 @@
 
-import './App.css'
+import Cards from './component/Cards'
+import Carousel from './component/Carousel'
+import Navbar from './component/Navbar'
 
 function App() {
 
   return (
     <>
-     
+    <div>
+    <Navbar/>
+    </div>
+    
+    <div>
+      <Carousel/>
+    </div>
+    
+    <div>
+<Cards/>
+    </div>
+
     </>
   )
 }
--- Redux-Ecommerce-App/src/main.jsx
+++ Redux-Ecommerce-App/src/main.jsx
@@ -3,10 +3,46 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './Config/Redux/store.js'
+import store from './Config/store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import { Home } from './Pages/Home.jsx'
+import About from './Pages/about.jsx'
+import Cart from './Pages/cart.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+
+    children:[
+    {
+      path: "",
+      element:<Home/>,
+    },
+    {
+      path:"about",
+      element:<About/>
+    },
+    {
+      path:"cart",
+      element: <Cart/>
+    },
+    {
+      path:"*",
+      element: <h2 className='p-2 mt-5 flex justify-center border-2 rounded'>Not found</h2>
+    },
+  ]
+}
+])
+
 
 createRoot(document.getElementById('root')).render(
    <Provider store={store}>
+    <RouterProvider router={router}>
      <App />
+    </RouterProvider>
    </Provider>
+
 )
