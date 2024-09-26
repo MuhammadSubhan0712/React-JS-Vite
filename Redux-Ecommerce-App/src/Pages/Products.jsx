e3790a493585cc10306536c455f524e5004baf89
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-27 02:16:09 +0500

    Ecommerce Products

--- Redux-Ecommerce-App/src/Pages/Products.jsx
+++ Redux-Ecommerce-App/src/Pages/Products.jsx
@@ -16,11 +16,11 @@ const Products = () => {
   return (
     <>
       {products ? (
-        <div className="bg-pink-300 mt-5 grid grid-cols-1 md:grid-cols-3 gap-8 p-7">
+        <div className="bg-red-200 mt-5 grid grid-cols-1 md:grid-cols-3 gap-8 p-7">
           {products.map((item) => (
             <div
               key={item.id}
-              className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl transition-all duration-300 ease-in-out dark:bg-gray-800 dark:border-gray-700"
+              className="w-full max-w-sm mt-5 bg-white border border-gray-200 rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl transition-all duration-300 ease-in-out dark:bg-gray-800 dark:border-gray-700"
             >
               <a href="#">
                 <img
@@ -64,7 +64,7 @@ const Products = () => {
                   </span>
                   <a
                     href="#"
-                    className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-transform duration-300 ease-in-out"
+                    className="text-white bg-blue-500 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-transform duration-300 ease-in-out"
                   >
                     Add to cart
                   </a>
